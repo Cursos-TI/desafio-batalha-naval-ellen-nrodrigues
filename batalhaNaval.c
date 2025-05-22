e0ebdadb6971878d723baf6dcccfae4ac4a068e6
Taniellen Rodrigues <tanyellens@gmail.com>
2025-05-22 10:41:38 -0300

    Adicionei habilidades
    Organizei o código, criei funções pros navios e adicionei habilidades com padrões no tabuleiro
    Dividi o código em funções pra posicionar navios horizontal e vertical. Criei uns padrões de habilidades (cone, cruz, octaedro) que marcam áreas do tabuleiro.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,67 +1,115 @@
 #include <stdio.h>
-int main() {
-    // Tamanho fixo do tabuleiro e dos navios
-    int tabuleiro[10][10];  // Matriz 10x10
-    int i, j;
+#include <stdlib.h>
 
-    // Inicializa todas as posições com 0 (água)
-    for (i = 0; i < 10; i++) {
-        for (j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0;
+#define TAMANHO_TABULEIRO 10
+#define TAMANHO_HABILIDADE 5
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int linha = 0; linha < TAMANHO_TABULEIRO; linha++) {
+        for (int coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++) {
+            tabuleiro[linha][coluna] = 0;
         }
     }
+}
 
-    // Coordenadas do navio horizontal (linha, coluna)
-    int linhaHorizontal = 6;
-    int colunaHorizontal = 2;
-
-    // Coordenadas do navio vertical (linha, coluna)
-    int linhaVertical = 0;
-    int colunaVertical = 9;
-
-    //Cordenadas do navio diagonal (linha, coluna)
-    // Navio diagonal principal 
-    int linhaDiagonal1 = 1;
-    int colunaDiagonal1 = 1;
-
-    // Navio diagonal secundária 
-    int linhaDiagonal2 = 2;
-    int colunaDiagonal2 = 7;
-
-    
-    int tamanhoNavio = 3;  // Cada navio tem 3 posições
-    
-
-   
-        // Coloca navio horizontal
-        for (i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linhaHorizontal][colunaHorizontal + i] = 3;
+void posicionarNavioHorizontal(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (coluna + i < TAMANHO_TABULEIRO) {
+            tabuleiro[linha][coluna + i] = 3;
         }
+    }
+}
 
-        // Coloca navio vertical
-        for (i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linhaVertical + i][colunaVertical] = 3;
+void posicionarNavioVertical(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna) {
+    for (int i = 0; i < 3; i++) {
+        if (linha + i < TAMANHO_TABULEIRO) {
+            tabuleiro[linha + i][coluna] = 3;
         }
+    }
+}
 
-        // Coloca navio diagonal principal
-        for (i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linhaDiagonal1 + i][colunaDiagonal1 + i] = 3;
-        }
+void aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int habilidade[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE], int origemLinha, int origemColuna) {
+    int deslocamento = TAMANHO_HABILIDADE / 2;
 
-        // Coloca navio diagonal secundária
-        for (i = 0; i < tamanhoNavio; i++) {
-            tabuleiro[linhaDiagonal2 + i][colunaDiagonal2 - i] = 3;
-        }
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            int linhaTabuleiro = origemLinha - deslocamento + i;
+            int colunaTabuleiro = origemColuna - deslocamento + j;
 
-        // Exibe o tabuleiro
-        printf("Tabuleiro final:\n\n");
-        for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
-                printf("%d ", tabuleiro[i][j]);
+            if (linhaTabuleiro >= 0 && linhaTabuleiro < TAMANHO_TABULEIRO && colunaTabuleiro >= 0 && colunaTabuleiro < TAMANHO_TABULEIRO) {
+                if (habilidade[i][j] == 1 && tabuleiro[linhaTabuleiro][colunaTabuleiro] == 0) {
+                    tabuleiro[linhaTabuleiro][colunaTabuleiro] = 5;
+                }
             }
-            printf("\n");
         }
-    
+    }
+}
+
+void criarMatrizCone(int cone[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE]) {
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            if (j >= TAMANHO_HABILIDADE / 2 - i && j <= TAMANHO_HABILIDADE / 2 + i) {
+                cone[i][j] = 1;
+            } else {
+                cone[i][j] = 0;
+            }
+        }
+    }
+}
+
+void criarMatrizCruz(int cruz[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE]) {
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            if (i == TAMANHO_HABILIDADE / 2 || j == TAMANHO_HABILIDADE / 2) {
+                cruz[i][j] = 1;
+            } else {
+                cruz[i][j] = 0;
+            }
+        }
+    }
+}
+
+void criarMatrizOctaedro(int octaedro[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE]) {
+    for (int i = 0; i < TAMANHO_HABILIDADE; i++) {
+        for (int j = 0; j < TAMANHO_HABILIDADE; j++) {
+            if (abs(i - TAMANHO_HABILIDADE / 2) + abs(j - TAMANHO_HABILIDADE / 2) <= TAMANHO_HABILIDADE / 2) {
+                octaedro[i][j] = 1;
+            } else {
+                octaedro[i][j] = 0;
+            }
+        }
+    }
+}
+
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int linha = 0; linha < TAMANHO_TABULEIRO; linha++) {
+        for (int coluna = 0; coluna < TAMANHO_TABULEIRO; coluna++) {
+            printf("%d ", tabuleiro[linha][coluna]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
+    int cone[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+    int cruz[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+    int octaedro[TAMANHO_HABILIDADE][TAMANHO_HABILIDADE];
+
+    inicializarTabuleiro(tabuleiro);
+
+    posicionarNavioHorizontal(tabuleiro, 2, 1);
+    posicionarNavioVertical(tabuleiro, 5, 4);
+
+    criarMatrizCone(cone);
+    criarMatrizCruz(cruz);
+    criarMatrizOctaedro(octaedro);
+
+    aplicarHabilidade(tabuleiro, cone, 3, 3);
+    aplicarHabilidade(tabuleiro, cruz, 6, 6);
+    aplicarHabilidade(tabuleiro, octaedro, 8, 2);
+
+    exibirTabuleiro(tabuleiro);
 
     return 0;
 }
